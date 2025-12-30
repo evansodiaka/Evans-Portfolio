d0bc6877001852004f8f9f1d1681deb832a91055
evans Odiaka <evans.odiaka@westernfg.ca>
2025-12-29 23:06:43 -0600

    fix: escape apostrophes in JSX for ESLint compliance

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -114,9 +114,9 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
-                I'm always open to discussing new projects, creative ideas or
+                I&apos;m always open to discussing new projects, creative ideas or
                 opportunities to be part of your visions.
               </p>
               
